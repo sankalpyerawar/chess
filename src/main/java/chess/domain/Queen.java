d7702041dca00c4c3cbf20ed413cbb3ffdb0eebd
Sankalp Yerawar <sankalpyerawar@gmail.com>
2025-08-18 23:15:57 +0530

    Refactor Queen to extend Piece instead of King and define valid moves

--- src/main/java/chess/domain/Queen.java
+++ src/main/java/chess/domain/Queen.java
@@ -1,6 +1,22 @@
 package chess.domain;
 
-public class Queen extends King {
+import java.util.List;
+
+public class Queen extends Piece {
+  private static final List<Step> POSSIBLE_STEPS = List.of(
+    new Step(1, 0),
+    new Step(-1, 0),
+    new Step(0, 1),
+    new Step(0, -1),
+    new Step(1, 1),
+    new Step(1, -1),
+    new Step(-1, 1),
+    new Step(-1, -1)
+  );
+
+  @Override
+  protected List<Step> getPieceSteps() { return POSSIBLE_STEPS; }
+
   @Override
   protected boolean isRepeating() {
     return true;
